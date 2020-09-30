6a4e2a14e276c4486178d3c0bc0274d421a85593
KasperBaun <kasperbaun@hotmail.com>
2020-09-30 10:57:08 +0200

    Removed unused and wrong header

--- myshell.c
+++ myshell.c
@@ -1,19 +1,22 @@
+#include "stdio.h"
+#include "myshell.h"
 
 
+int int main(int argc, char const *argv[]) {
+  #define TRUE1
 
+  while(TRUE){
+      type_prompt(); /* This displays the prompt */
+      read_command(command, parameters); /* Reads the user input from terminal */
 
-#define TRUE1
+      if (fork() !=0){ /* fork off child process */
 
-while(TRUE){
-    type_prompt(); /* This displays the prompt */
-    read_command(command, parameters); /* Reads the user input from terminal */
-    
-    if (fork() !=0){ /* fork off child process */
-      
-      /* Parent code */
-      waitpid(-1, &status, 0); /* Wait for child to exit*/
-      } else {
-        /* Child code */
-        execve(command, parameters, 0); /* Child executes the user input command */
-        }
- }
+        /* Parent code */
+        waitpid(-1, &status, 0); /* Wait for child to exit*/
+        } else {
+          /* Child code */
+          execve(command, parameters, 0); /* Child executes the user input command */
+          }
+   }
+  return 0;
+}
