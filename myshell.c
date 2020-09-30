afb63a80af10db9bfe92a4cd95297ea4546523c7
Kasper Baun <43874899+KasperBaun@users.noreply.github.com>
2020-09-30 09:48:21 +0200

    Create myshell.c
    Added almost empty shell

---
+++ myshell.c
@@ -0,0 +1,19 @@
+
+
+
+
+#define TRUE1
+
+while(TRUE){
+    type_prompt(); /* This displays the prompt */
+    read_command(command, parameters); /* Reads the user input from terminal */
+    
+    if (fork() !=0){ /* fork off child process */
+      
+      /* Parent code */
+      waitpid(-1, &status, 0); /* Wait for child to exit*/
+      } else {
+        /* Child code */
+        execve(command, parameters, 0); /* Child executes the user input command */
+        }
+ }
