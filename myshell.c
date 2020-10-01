29f7eaa8eb4693fd9e7aaaacff30201e558afd02
KasperBaun <kasperbaun@hotmail.com>
2020-10-01 11:00:18 +0200

    shell prompt working - able to take commands and exitExit input

--- myshell.c
+++ myshell.c
@@ -1,22 +1,86 @@
-#include "stdio.h"
 #include "myshell.h"
 
+int main(int argc, char const *argv[]) {
 
-int int main(int argc, char const *argv[]) {
-  #define TRUE1
+int shellLoaded = 1;
+int status=1;
+char *command;
+char*const* parameters;
 
-  while(TRUE){
-      type_prompt(); /* This displays the prompt */
-      read_command(command, parameters); /* Reads the user input from terminal */
 
-      if (fork() !=0){ /* fork off child process */
-
-        /* Parent code */
-        waitpid(-1, &status, 0); /* Wait for child to exit*/
-        } else {
-          /* Child code */
-          execve(command, parameters, 0); /* Child executes the user input command */
-          }
-   }
-  return 0;
+// Clears the console if the program has just started and displays welcomemessage
+if(shellLoaded){
+  clearScreen();
+  shellLoaded=0;
+  displayWelcome();
 }
+
+
+while(1){
+// Shows the prompt and takes input from user
+  command = show_prompt();
+// Reads input from user then decides where to go from there.
+  read_command(command,parameters);
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Create new childprocess.
+   If succesful, execute command with parameters  in the child.
+   If unsuccesfull - wait for child to terminate.
+if (fork() !=0){
+    waitpid(-1, &status, 0);
+    } else {
+          execve(command, parameters, 0);
+          show_prompt();
+    }
+}
+*/
+
+/*
+while(TRUE){
+
+    if (fork() !=0){                    // fork off child process
+
+      // Parent code
+      waitpid(-1, &status, 0);          // Wait for child to exit
+      } else {
+              // Child code
+              execve(command, parameters, 0); // Child executes the user input command
+        }
+ }
+ */
--- myshell.h
+++ myshell.h
@@ -1,1 +1,49 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "string.h"
 
+
+
+// TODO: Make prompt take more than a string input (parameters)
+
+
+// show_prompt() - Shows the prompt with blinking cursor ready to take input
+char* show_prompt() {
+  char *command = malloc(sizeof(char)*1024);
+  printf("%s", ">>> ");
+  scanf("%s", command);
+  return command;
+}
+
+void clearScreen() {
+  system("clear");
+}
+void read_command(char* command, char*const* parameters) {
+
+  if(strcmp(command,"exit")==0 | strcmp(command, "Exit")==0){
+    // Closes all current running processes. Child processes are sent SIGCHLD signal to terminate.
+    printf("%s\n","Goodbye!" );
+    exit(0);
+  }
+
+  if(strcmp(command,"commands")==0 | strcmp(command,"Commands")==0){
+    printf("%s\n","1. Type 'pipe' for blablabla");
+    printf("%s\n","1. Type 'pipe' for blablabla");
+    printf("%s\n","1. Type 'pipe' for blablabla");
+    printf("%s\n","1. Type 'pipe' for blablabla");
+    printf("%s\n","1. Type 'pipe' for blablabla");
+    printf("%s\n","1. Type 'pipe' for blablabla");
+    printf("%s\n","1. Type 'pipe' for blablabla");
+    printf("%s\n","1. Type 'pipe' for blablabla");
+    printf("%s\n","1. Type 'exit/Exit' to exit the shell");
+  }
+
+}
+void displayWelcome(){
+  printf("%s\n","##########################################################");
+  printf("%s\n","##         Welcome to s172483's Simple Shell            ##");
+  printf("%s\n","##         For a list of commands type: 'commands'      ##");
+  printf("%s\n","##         To exit: Ctrl+Z or type exit/Exit            ##");
+  printf("%s\n","##########################################################");
+}
